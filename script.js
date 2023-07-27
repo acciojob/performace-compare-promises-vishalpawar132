fee43834f96344417b9bd4801829650a37cb258a
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-27 09:03:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,46 @@ const apiUrls = [
 ];
 
 // You can write your code here
+const fetchData = async (url) => {
+  const startTime = performance.now();
+  const response = await fetch(url);
+  const data = await response.json();
+  const endTime = performance.now();
+  return endTime - startTime;
+};
+
+// Function to fetch data from multiple APIs using Promise.all
+const fetchWithPromiseAll = async () => {
+  try {
+    const start = performance.now();
+    const promises = apiUrls.map(fetchData);
+    const times = await Promise.all(promises);
+    const totalTime = performance.now() - start;
+
+    // Display the results on the webpage
+    const outputAll = document.getElementById('output-all');
+    outputAll.textContent = `${totalTime.toFixed(2)} ms`;
+  } catch (error) {
+    console.error('Error with Promise.all:', error);
+  }
+};
+
+// Function to fetch data from multiple APIs using Promise.any
+const fetchWithPromiseAny = async () => {
+  try {
+    const start = performance.now();
+    const promises = apiUrls.map(fetchData);
+    await Promise.any(promises);
+    const totalTime = performance.now() - start;
+
+    // Display the results on the webpage
+    const outputAny = document.getElementById('output-any');
+    outputAny.textContent = `${totalTime.toFixed(2)} ms`;
+  } catch (error) {
+    console.error('Error with Promise.any:', error);
+  }
+};
+
+// Call the functions to fetch data and display the results
+fetchWithPromiseAll();
+fetchWithPromiseAny();
